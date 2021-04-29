b527fab6fdf928938430468f63f12fe759a8fcd9
Tejas Mehta <tmthecoder@gmail.com>
2021-04-29 01:16:07 -0400

    add the custom route observer

--- lib/controller_widgets.dart
+++ lib/controller_widgets.dart
@@ -3,5 +3,7 @@ library controller_widgets;
 export 'routing/route_controller.dart';
 export 'routing/route_controller_widget.dart';
 
+export 'routing/custom_route_observer.dart';
+
 export 'theming/theme_controller.dart';
 export 'theming/theme_controller_widget.dart';
